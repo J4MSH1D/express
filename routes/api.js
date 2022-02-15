3016ea8f0abd24ef006c4d0b41e5ee3ed83d73cd
Jamshid Abduqodirov <jamshidabdukadirov01@gmail.com>
2022-02-15 15:20:56 +0500

    patch and delete requests are added

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router()
 const Post = require("../models/post")
-
+// Get all posts
 routes.get("/posts", async (req, res) => {
     try {
         const list = await Post.find()
@@ -9,7 +9,7 @@ routes.get("/posts", async (req, res) => {
         res.send(err.message);
     }
 })
-
+// Get specific posts
 routes.get("/posts/:id", async (req, res) => {
     try {
         const users = await Post.findById(req.params.id)
@@ -18,7 +18,7 @@ routes.get("/posts/:id", async (req, res) => {
         res.send(err.message);
     }
 })
-
+// To post a new post
 routes.post("/posts", async (req, res) => {
     const newPost = new Post ({
         name: req.body.name,
@@ -33,7 +33,7 @@ routes.post("/posts", async (req, res) => {
     }
 })
 
-
+// To delete specific post
 routes.delete("/posts/:id", async (req, res) => {
     try {
         const deletedUser = await Post.deleteOne({_id: req.params.id})
@@ -42,7 +42,22 @@ routes.delete("/posts/:id", async (req, res) => {
         res.json(err)
     }
 })
+// To change specific post
+routes.patch("/posts/:id", async (req, res)=> {
+    try {
+        const patched = await Post.updateOne(
+            {
+                _id: req.params.id
+            },
+            {
+                $set: { name: req.body.name, password: req.body.password, email: req.body.email }
+            }
+        )
+        res.json(patched)
+    } catch(err) {
+        res.json(err)
+    }
+})
 
-
-
+// Exporting all
 module.exports = routes
